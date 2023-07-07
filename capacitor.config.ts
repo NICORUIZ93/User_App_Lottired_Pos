715ff6801902fceb504483b911fffad3e11941f7
Nicolás Ruiz <NICOLASENRIQUERUIZ93@HOTMAIL.COM>
2023-07-06 21:46:05 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'User-App-Lottired-Pos',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
