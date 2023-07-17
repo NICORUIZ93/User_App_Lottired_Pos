db81bb188def9ea709ace4bef01f9033070b1af9
Nicolás Ruiz <NICOLASENRIQUERUIZ93@HOTMAIL.COM>
2023-07-17 13:14:56 -0500

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-import { HTTP } from '@awesome-cordova-plugins/http/ngx';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page404Component } from './pages/static/page404/page404.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { JwtInterceptor } from 'src/services/interceptors/jwt-interceptor';
 
 @NgModule({
   declarations: [AppComponent, Page404Component],
@@ -22,11 +19,7 @@ import { JwtInterceptor } from 'src/services/interceptors/jwt-interceptor';
     FormsModule,
     ReactiveFormsModule,
   ],
-  providers: [
-    HTTP,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
